4c921d0dc5c07616f988373e5e7f6dbbf18480ec
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-07-05 00:26:31 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
     lang: 'en-US',
     title: 'Sorex Studio',
     description: 'Community',
+    port: '8080',
     themeConfig: {
         // LOGO
         logo: 'https://media.discordapp.net/attachments/760210364008366200/826516927367217222/LogoPNG3.png',
