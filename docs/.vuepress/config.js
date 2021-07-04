f94eaa882521204b6af5bb87dc56f144aa58b54f
Nico Leon Wien <nlwien3007@gmail.com>
2021-07-04 16:34:37 +0200

    Changed config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,6 @@ module.exports = {
     lang: 'en-US',
     title: 'Hello, VuePress!',
     description: 'This is my first VuePress site',
-    base: '/sorexproject.eu/',
-  
     themeConfig: {
       logo: 'https://vuejs.org/images/logo.png',
     },
