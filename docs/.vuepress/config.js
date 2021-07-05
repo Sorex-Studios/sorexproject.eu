f3b6035372a9142ee12a9a29a11a46431fcf5669
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-07-05 16:15:09 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,24 +4,33 @@ module.exports = {
     description: 'Community',
     port: '8080',
     head: [
-        ['link', { rel: 'icon', href: '/LogoPNG3.png' }]
+        ['link', { rel: 'icon', href: '/logo.png' }]
     ],
     themeConfig: {
         activeHeaderLinks: false,
         // LOGO
-        logo: '/LogoPNG3.png',
+        logo: '/logo.png',
         // default value is true. Set it to false to hide next page links on all pages
         nextLinks: true,
         // default value is true. Set it to false to hide prev page links on all pages
         prevLinks: true,
         // You can disable the built-in search box with themeConfig.search: false, and customize how many suggestions will be shown with themeConfig.searchMaxSuggestions:
-        search: true,
-        searchMaxSuggestions: 10,
+        // search: true,
+        // searchMaxSuggestions: 10,
         // You can add links to the navbar via themeConfig.nav:
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' }
+        navbar: [
+            // NavbarItem
+            {
+                text: 'Home',
+                link: '/',
+            },
+            // NavbarGroup
+            {
+                text: 'Group',
+                children: ['/group/foo.md', '/group/bar.md'],
+            },
+            // string - page file path
+            '/bar/README.md',
         ],
         // The sidebar automatically displays links for headers in the current active page, nested under the link for the page itself. You can customize this behavior using themeConfig.sidebarDepth. The default depth is 1, which extracts the h2 headers. Setting it to 0 disables the header links, and the max value is 2 which extracts both h2 and h3 headers.
         sidebarDepth: 2,
