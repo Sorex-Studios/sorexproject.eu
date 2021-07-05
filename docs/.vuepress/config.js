6f58f57e50518e9f5c811f0877a8cf052f9a337e
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-07-05 16:50:28 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,12 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/logo.png' }]
     ],
+    theme: '@vuepress/theme-default',
     themeConfig: {
         activeHeaderLinks: false,
         // LOGO
         logo: '/logo.png',
-        // default value is true. Set it to false to hide next page links on all pages
+        // default value is true. Set it ton false to hide next page links on all pages
         nextLinks: true,
         // default value is true. Set it to false to hide prev page links on all pages
         prevLinks: true,
@@ -24,26 +25,7 @@ module.exports = {
                 text: 'Home',
                 link: '/',
             },
-            // NavbarGroup
-            {
-                text: 'Group',
-                children: ['/group/foo.md', '/group/bar.md'],
-            },
-            // string - page file path
-            '/bar/README.md',
         ],
-        // The sidebar automatically displays links for headers in the current active page, nested under the link for the page itself. You can customize this behavior using themeConfig.sidebarDepth. The default depth is 1, which extracts the h2 headers. Setting it to 0 disables the header links, and the max value is 2 which extracts both h2 and h3 headers.
-        sidebarDepth: 2,
-        sidebar: [{
-            title: 'Group 1', // required
-            path: '/foo/', // optional, link of the title, which should be an absolute path and must exist
-            collapsable: false, // optional, defaults to true
-            children: [
-                '/',
-                '/T6ZB/Bank.html'
-
-            ]
-        }],
     }
 
 }
